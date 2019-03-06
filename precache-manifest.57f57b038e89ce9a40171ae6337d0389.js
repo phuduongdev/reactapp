07efca1a21ca61d7b8367e3dd0f271268d716a00
phu <phu.duongtan@gmail.com>
2019-03-06 17:28:52 +0700

    Updates

---
+++ precache-manifest.57f57b038e89ce9a40171ae6337d0389.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "03ea28fa17661fe787d4",
+    "url": "/reactapp/static/js/runtime~main.03ea28fa.js"
+  },
+  {
+    "revision": "23603828ea55a7a88162",
+    "url": "/reactapp/static/js/main.23603828.chunk.js"
+  },
+  {
+    "revision": "e322be143ade21a3c597",
+    "url": "/reactapp/static/js/2.e322be14.chunk.js"
+  },
+  {
+    "revision": "21cb496ad05da74d40072f0759a5605b",
+    "url": "/reactapp/index.html"
+  }
+];
